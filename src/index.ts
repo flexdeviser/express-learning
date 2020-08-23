2481d0cbe4a9626d74e7a9d1665d4e19c76f0aa0
Eric Wang <eric@future-grid.com>
2020-08-23 18:05:29 +1000

    init load

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import express, { Application, NextFunction, Request, Response } from 'express';
+
+type ServerProps = {
+  port: number;
+  routes: Array<any>;
+};
+
+/**
+ **/
+const start = ({ port, routes }: ServerProps) => {
+  const app = express();
+  routes.map((_route) => app.use(_route.path, _route.handler));
+  app.listen(port, () => console.log(`Server is listening on port ${port}!`));
+  return app;
+};
+
+const routes: Array<any> = [
+  {
+    path: '/',
+    handler: (req: Request, res: Response, next: NextFunction) => {
+      res.status(200).send({ data: 'Hello from Eric!' });
+    },
+  },
+];
+
+// Boot express
+start({ port: 5000, routes: routes });
